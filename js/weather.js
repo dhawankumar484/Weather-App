215cef1180da5c0a4ccf6b87a7e1483fc1eec0c6
Dhawan <dhawan.kumar_cs19@gla.ac.in>
2023-07-27 19:19:06 +0530

    changes in js file

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,21 @@
 const api = {
-    key: "cb1437b6f56e574ea5f69478397d5c98",
-    base: "https://api.openweathermap.org/data/2.5/"
-  };
+  key: "49a1e155ccd390da2017f2bbe25c3a42",
+  base: "https://api.openweathermap.org/data/2.5/"
+};
 
 const searchbox = document.querySelector(".search-box");
 searchbox.addEventListener("keypress", setQuery);
+
+function setQuery(evt) {
+  if (evt.keyCode == 13) {
+    getResults(searchbox.value) ;
+  }
+}
+
+function getResults(query) {
+  fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+    .then((weather) => {
+      return weather.json();
+    })
+    .then(displayResults);
+}
